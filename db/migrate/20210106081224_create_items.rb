85e64a3a5fb29bd2af15b666187307c5f890a1d8
haruru003 <u0yuu06@gmail.com>
2021-01-06 17:56:24 +0900

    migraetion fileの修正

---
+++ db/migrate/20210106081224_create_items.rb
@@ -0,0 +1,16 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :item_name
+      t.text :introduction
+      t.integer :category_id
+      t.integer :item_condition_id
+      t.integer :postage_payer_id
+      t.integer :prefecture_code_id
+      t.integer :preparation_day_id
+      t.integer :price
+      t.references :user
+      t.timestamps
+    end
+  end
+end
